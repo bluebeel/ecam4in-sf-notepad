065c3297b7a30635a9ff73dcf037394d80484074
Bluebeel <saikouah@gmail.com>
2018-02-09 23:21:16 +0100

    Update add cat form

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -12,9 +12,43 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Entity\Category;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CategoryController extends Controller
 {
+    /**
+     * @Route("/categories/new", name="new_category")
+     * @param mixed $request
+     * @return mixed
+     */
+    public function newCategory(Request $request)
+    {
+        $category = new Category();
+        $category->setLibelle('Write a libelle');
+
+        $form = $this->createFormBuilder($category)
+            ->add('libelle', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Create Category'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            $category = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute('notes');
+        }
+
+        return $this->render('category/newCategory.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
     /**
      * @Route("/categories/{id}", name="category_show")
      * @param mixed $category
@@ -27,16 +61,6 @@ class CategoryController extends Controller
         return $this->render('note/index.html.twig', array(
             'notes' => $notes,
         ));
-    }
-
-    /**
-     * @Route("/categories/new", name="new_category")
-     * @param mixed $request
-     * @return mixed
-     */
-    public function newCategory(Request $request)
-    {
-
     }
 }
 ?>
