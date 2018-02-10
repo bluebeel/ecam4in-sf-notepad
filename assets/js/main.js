e742955aba26e0d822f3d4fe3b20c228b6c43d05
Bluebeel <saikouah@gmail.com>
2018-02-10 13:55:30 +0100

    Add active class to menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,15 @@ var $ = require('jquery');
 // JS is equivalent to the normal "bootstrap" package
 // no need to set this to a variable, just require it
 require('bootstrap');
+
+$(function(){
+    var current = location.pathname;
+    console.log(current);
+    $('.list-group a').each(function(){
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if($this.attr('href') === current){
+            $this.addClass('active');
+        }
+    })
+});
