c9d326c74078e4722e0e696ff4233eaf6c1e0e61
bluebeela <saikouah@gmail.com>
2018-04-18 17:20:10 +0200

    Update notes and note component

--- frontend/src/app/note/note.component.ts
+++ frontend/src/app/note/note.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Observable } from 'rxjs/Observable';
+import { NoteService } from '../note.service';
+import { Note } from '../note';
 
 @Component({
   selector: 'app-note',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteComponent implements OnInit {
 
-  constructor() { }
+  note: Note;
+
+  constructor(private route: ActivatedRoute,
+              private noteService: NoteService,
+              private location: Location) { }
 
   ngOnInit() {
+    this.getNote();
+  }
+
+  getNote(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.noteService.getNoteById(id)
+      .subscribe(note => this.note = note);
   }
 
 }
